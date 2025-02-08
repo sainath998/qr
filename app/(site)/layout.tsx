ef9fef9e1684abbea733ac196382cf1d6c9280e3
Sainath <sainath@Sainaths-MacBook-Pro-3.local>
2025-02-09 03:26:12 +0530

    dsfsaf

---
+++ app/(site)/layout.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Lines from "@/components/Lines";
+import ScrollToTop from "@/components/ScrollToTop";
+import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
+import "../globals.css";
+const inter = Inter({ subsets: ["latin"] });
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+import ToasterContext from "../context/ToastContext";
+
+
+const clientId =
+  "561932436702-5h1ci89mosgjdu6j1sraokaip98qvbnv.apps.googleusercontent.com";
+
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`dark:bg-black ${inter.className}`}>
+      <GoogleOAuthProvider clientId={clientId}>
+        <ThemeProvider
+          enableSystem={false}
+          attribute="class"
+          defaultTheme="light"
+        >
+          <Lines />
+          <Header />
+          <ToasterContext />
+          {children}
+          <Footer />
+          <ScrollToTop />
+        </ThemeProvider>
+      </GoogleOAuthProvider>
+      </body>
+    </html>
+  );
+}
